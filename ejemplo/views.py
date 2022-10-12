f39814b4af2b56a6b5b4c33d4ce62ad7c4619adc
Germán Martinez <germand_m@hotmail.com>
2022-10-11 23:26:43 -0300

    agrego ejemplos de clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    suma = 1 + 1
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre": "German",
+        "apellido": suma,
+    })
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre":nombre,
+        "apellido": apellido,
+    })
+
+def index_tres(request):
+
+    return render(request,  "ejemplo/saludar.html", 
+    {"notas": [1,2,3,4,5,6,7,8]}
+    )
+
+def imc(request, peso, altura):
+    imc = 1 # caluclar el imc
+    return render(request, "ejemplo/imc.html", {"imc": imc})
+
