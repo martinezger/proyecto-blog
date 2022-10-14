5ec76395ce2ed60d9e42daee6b2c01b2336b15ee
Germán Martinez <germand_m@hotmail.com>
2022-10-13 22:58:10 -0300

    agregure la funcionalidad imc. agregue modelo familar

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     suma = 1 + 1
@@ -22,6 +23,16 @@ def index_tres(request):
     )
 
 def imc(request, peso, altura):
-    imc = 1 # caluclar el imc
+    altura_en_metros = altura / 100 
+    peso_en_kilos = peso / 100
+    imc = peso_en_kilos / altura_en_metros * altura_en_metros
+
+
     return render(request, "ejemplo/imc.html", {"imc": imc})
+
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", 
+                {"lista_familiares": lista_familiares})
+
 
--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index, index_dos, index_tres
+from ejemplo.views import (index, index_dos, index_tres, 
+                           imc, monstrar_familiares)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('saludar/', index),
      path('saludar/<nombre>/<apellido>/', index_dos),
      path('mostrar-notas/', index_tres),
+     path('imc/<int:peso>/<int:altura>', imc),
+     path('mi-familia/', monstrar_familiares),
 ]
