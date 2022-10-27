f250629b8a828ebbeddd03ec365c5c18f43352c1
Germán Martinez <germand_m@hotmail.com>
2022-10-27 20:54:21 -0300

    autenticación de usuario

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
+#nuevos imports#
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+#nuevos imports#
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from blog.models import Post
 
+@login_required
 def index(request):
     return render(request, 'blog/index.html')
 
-class ListPost(ListView):
+class ListPost(LoginRequiredMixin, ListView):
     model=Post
 
 class CreatePost(CreateView):
@@ -34,3 +40,10 @@ class SearchPostByName(ListView):
         return Post.objects.filter(title__icontains=blog_title)
 
 
+class BlogLogin(LoginView):
+    template_name = 'blog/blog_login.html'
+    next_page = reverse_lazy("list-post")
+
+class BlogLogout(LogoutView):
+    template_name = 'blog/blog_logout.html'
+    
